8e5f5e5edd7765ccf02637b42872be1b8d2c251a
Harish Balaji <harish.b@arrise.com>
2026-02-26 23:51:57 +0530

    Initialized new project | Initial setup to accept websocket connection | Deleted unwanted files

---
+++ backend/src/main/java/com/racoons/app/RacoonsAppApplication.java
@@ -0,0 +1,13 @@
+package com.racoons.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacoonsAppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RacoonsAppApplication.class, args);
+	}
+
+}
