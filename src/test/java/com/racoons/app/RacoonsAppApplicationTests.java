e69da9cf24773ae6315cadbb02b7e434a82d5254
Harish Balaji <harish.b@arrise.com>
2026-03-29 17:22:29 +0530

    Created dummy websocket listener

---
+++ src/test/java/com/racoons/app/RacoonsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.racoons.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacoonsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
